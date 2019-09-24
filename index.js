bc30bcb9e0cc96f3634fba52e5bd282548b25eb4
Skelman50 <skelman50@gmail.com>
2019-09-24 21:05:47 +0300

    select el with attributes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 async function main() {
-  const html = await request("https://reactnativetutorial.net/css-selectors/");
+  const html = await request(
+    "https://reactnativetutorial.net/css-selectors/lesson5.html"
+  );
   const $ = await cheerio.load(html);
-  const text = $("h1").text();
-  console.log(text);
+  $(".red").each((i, element) => {
+    console.log($(element).text().trim());
+  });
 }
 
 main();

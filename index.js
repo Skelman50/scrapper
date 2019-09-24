7896fbf4030fcb1930ec20f9580b234e63e070f9
Skelman50 <skelman50@gmail.com>
2019-09-24 19:45:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+async function main() {
+  const html = await request("https://reactnativetutorial.net/css-selectors/");
+  const $ = await cheerio.load(html);
+  const text = $("h1").text();
+  console.log(text);
+}
+
+main();

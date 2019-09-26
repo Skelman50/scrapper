de473f9b6f6a21595282718dcdf26de530d3cfd6
Skelman50 <skelman50@gmail.com>
2019-09-26 14:31:40 +0300

    scrap job descriptions

--- index.js
+++ index.js
@@ -14,23 +14,54 @@ const scrapingTesult = [
   }
 ];
 
-const main = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+const hoodReplaceTrim = string =>
+  string
+    .text()
+    .trim()
+    .replace("(", "")
+    .replace(")", "");
+
+const sleep = milliseconds => {
+  return new Promise(res => setTimeout(res, milliseconds));
+};
+
+const scraperListings = async page => {
   await page.goto("https://sfbay.craigslist.org/search/sof");
   const html = await page.content();
   const $ = cheerio.load(html);
-  const results = $(".result-info")
+  const listings = $(".result-info")
     .map((_, element) => {
       const titleElement = $(element).find(".result-title");
       const dateElement = $(element).find(".result-date");
+      const hood = $(element).find(".result-hood");
+      const neighborhood = hoodReplaceTrim($(hood));
       const title = $(titleElement).text();
       const url = $(titleElement).attr("href");
       const datePost = new Date($(dateElement).attr("datetime"));
-      return { title, url, datePost };
+      return { title, url, datePost, neighborhood };
     })
     .get();
-  console.log(results);
+  return listings;
+};
+
+const scrapJobDescription = async (listings, page) => {
+  console.log(listings);
+  for (let i = 0; i < listings.length; i++) {
+    await page.goto(listings[i].url);
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const postDescription = $("#postingbody").text();
+    listings[i].jobDescription = postDescription;
+    console.log(listings[i].jobDescription);
+    await sleep(1000);
+  }
+};
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const listings = await scraperListings(page);
+  const listingsWithJobDescription = await scrapJobDescription(listings, page);
 };
 
 main();

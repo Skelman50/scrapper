6c073490aa80da03b6564dae4c546ce1f64b868d
Skelman50 <skelman50@gmail.com>
2019-09-30 10:48:58 +0300

    end imdb scrapper

--- imdb-scrapper/index.js
+++ imdb-scrapper/index.js
@@ -1,5 +1,10 @@
 const request = require("request-promise");
+const reqularRequest = require("request");
+const fs = require("fs");
+const path = require("path");
 const cheerio = require("cheerio");
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({ show: true, electronPath: require("electron") });
 
 const scrapTitleAndRatings = async () => {
   const html = await request.get(
@@ -24,10 +29,29 @@ const scrapTitleAndRatings = async () => {
   return movies.filter(m => m.title);
 };
 
+const getPosterImageUrl = async movies => {
+  for (let i = 0; i < movies.length; i++) {
+    try {
+      const posterImageUrel = await nightmare
+        .goto(movies[i].posterUrl)
+        .evaluate(() =>
+          $(
+            "#photo-container > div > div:nth-child(3) > div > div.pswp__scroll-wrap > div.pswp__container > div:nth-child(2) > div > img:nth-child(2)"
+          ).attr("src")
+        );
+      movies[i].posterImageUrel = posterImageUrel;
+      savePostersToDisk(movies[i]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return movies;
+};
+
 const getPostUrl = async movie => {
   const html = await request.get(movie.urlDescription);
   const $ = await cheerio.load(html);
-  movie.posterUrl = $("div.poster > a").attr("href");
+  movie.posterUrl = "https://www.imdb.com" + $("div.poster > a").attr("href");
   return movie;
 };
 
@@ -41,10 +65,19 @@ async function main() {
   try {
     let movies = await scrapTitleAndRatings();
     movies = await scrapPosterUrl(movies);
+    movies = await getPosterImageUrl(movies);
     console.log(movies);
   } catch (error) {
     console.log(error);
   }
+}
+
+function savePostersToDisk(movie) {
+  reqularRequest
+    .get(movie.posterImageUrel)
+    .pipe(
+      fs.createWriteStream(path.resolve(__dirname, `posters/${movie.rank}.png`))
+    );
 }
 
 main();
